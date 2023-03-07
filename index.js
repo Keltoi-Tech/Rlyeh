245b05744fe752bc79e17e797d37cb86543a8268
KeltoiCode <keltoitech@gmail.com>
2023-03-06 23:35:27 -0300

    updating npm info
    Update npm package.json and adding cthulhu method on index exporting

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export {kek} from './utils';
-export {HtmlComponent,define,component,trait} from './component';
+export {HtmlComponent,define,component,trait,cthulhu} from './component';
 export {HtmlMeta} from './html-meta';
 export {Routing} from './routing';
 export {entry,view,compose,head} from './view';
